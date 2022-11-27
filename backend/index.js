594d9524064060194192a4c4b6585f880a24ab09
liu-kevin1 <orgwirelist123@gmail.com>
2022-11-27 11:47:26 -0500

    initial credentials separation

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,40 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
 
 const app = express();
 const port = 5000;
 
+var credentialsFileName = "./credentials.json";
+var credentials;
+
+try {
+    credentials = require(credentialsFileName)
+}
+catch (err) {
+    credentials = {}
+    console.log("unable to read file '" + credentialsFileName + "': ", err)
+}
+
+console.log("session secret is:", credentials)
+
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "",
-  user: "",
-  password: ""
+    host: "",
+    user: "",
+    password: ""
 });
 
 app.use(cors());
 
-app.get('/', (req, res)=> res.send("Home"));
+app.get('/', (req, res) => res.send("Home"));
 app.get('/hello', (req, res) => res.send("hello"));
 
 // for MySQL
 app.get('/getSQLData', (req, res) => {
     console.log("this has ran");
-    con.connect(function(err) {
+    con.connect(function (err) {
         console.log("connecting...");
         if (err) {
             throw err;
@@ -29,7 +43,7 @@ app.get('/getSQLData', (req, res) => {
     });
     return res.send("yes");
 });
-app.post('/writeSQLData', (req, res) =>{
+app.post('/writeSQLData', (req, res) => {
     return;
 })
 
@@ -37,9 +51,10 @@ app.post('/writeSQLData', (req, res) =>{
 app.get('/getGSData', (req, res) => {
     return;
 });
-app.post('/writeGSData', (req, res) =>{
+app.post('/writeGSData', (req, res) => {
     return;
 })
 
 app.listen(port, () => {
-    console.log(`Listening to port ${port}!`)})
+    console.log(`Listening to port ${port}!`)
+})
